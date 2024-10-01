dd27016c853ff94959020fc6ac0c5be564e08c9b
MayurSongire19 <mayursongire00@gmail.com>
2024-10-01 13:12:14 +0530

    first commit

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using EmployeeManagementCRUD.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace EmployeeManagementCRUD.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+
+        }
+
+        public DbSet<Employee> Employees { get; set; }  
+    }
+}
---
+++ Models/Employee.cs
@@ -0,0 +1,14 @@
+﻿namespace EmployeeManagementCRUD.Models
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string MiddleName { get; set; }
+        public string LastName { get; set; }
+        public string PhoneNumber { get; set; }
+        public string EmailAddress { get; set; }
+        public string Gender { get; set; }
+        public string Country { get; set; }
+    }
+}
